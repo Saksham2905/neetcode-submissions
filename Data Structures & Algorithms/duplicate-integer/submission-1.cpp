f7e591bb4f6ea214bf3b269e5cc9f4d6ef3b891a
Saksham Gupta <sakshamgupta292005@gmail.com>
2026-07-02 05:57:26 +0530

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto num : nums){
+            mp[num]++;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(mp[nums[i]] > 1) return true;
+        }
+        return false;
+    }
+};

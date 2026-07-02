4e99bff008aea32580f59be5949a36a64bd9c908
Saksham Gupta <sakshamgupta292005@gmail.com>
2026-07-02 09:40:16 +0530

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int solve(int idx, vector<int>&cost, vector<int>&dp){
+        int n = cost.size();
+        if(idx >= n) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        return dp[idx] = cost[idx] + min(solve(idx+1, cost, dp), solve(idx+2, cost, dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n , -1);
+        return min(solve(0, cost, dp), solve(1, cost, dp));
+    }
+};
